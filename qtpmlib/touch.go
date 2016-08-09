dda8eefcd181d86ed44c73ac2fba0aaccae72889
Yoshiki Shibukawa <shibukawa.yoshiki@dena.jp>
2016-08-09 20:52:07 +0900

    fix touch command. now its regenerates CMakeLists of all child packages

--- qtpmlib/touch.go
+++ qtpmlib/touch.go
@@ -2,14 +2,29 @@ package qtpm
 
 import (
 	"github.com/fatih/color"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 )
 
-func Touch(config *PackageConfig, debug, verbose bool) {
-	_, err := BuildPackage(config, config, true, debug, false, !config.IsApplication)
-	if err != nil {
-		color.Red("\nBuild Error: %v\n", err)
-		os.Exit(1)
+func Touch(rootConfig *PackageConfig, debug, verbose bool) {
+	projects := []*PackageConfig{
+		rootConfig,
+	}
+	dirs, _ := ioutil.ReadDir(filepath.Join(rootConfig.Dir, "vendor"))
+	for _, dir := range dirs {
+		config, err := LoadConfig(filepath.Join(rootConfig.Dir, "vendor", dir.Name()), false)
+		if err == nil {
+			projects = append(projects, config)
+		}
+	}
+	for _, project := range projects {
+		printSubSection("Refreshing %s\n", project.Name)
+		_, err := BuildPackage(rootConfig, project, true, debug, false, !project.IsApplication)
+		if err != nil {
+			color.Red("\nTouch Error: %v\n", err)
+			os.Exit(1)
+		}
 	}
 	if verbose {
 		printSuccess("\nFinish updating CMakeLists.txt successfully\n")
